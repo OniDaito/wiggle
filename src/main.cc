259ea055b4b9266168bba569eaee8a8ea41bf859
Benjamin Blundell <me@benjamin.computer>
2021-05-19 18:12:16 +0100

    First commit

---
+++ src/main.cc
@@ -0,0 +1,26 @@
+/**
+ * ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * █░███░██▄██░▄▄▄█░▄▄▄█░██░▄▄
+ * █▄▀░▀▄██░▄█░█▄▀█░█▄▀█░██░▄▄
+ * ██▄█▄██▄▄▄█▄▄▄▄█▄▄▄▄█▄▄█▄▄▄
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ * @file main.cc
+ * @author Benjamin Blundell - k1803390@kcl.ac.uk
+ * @date 19/05/2021
+ * @brief Parse our tiff files and create our masks
+ * 
+ * Given a directory of worm information, create the masks
+ * we need.
+ *
+ */
+
+#include <masamune_prog.h>
+
+int main (int argc, char ** argv) {
+   
+    //return EXIT_FAILURE;
+    
+
+    return EXIT_SUCCESS;
+
+}
