d918edcf4b33b59f1b640baa799a74f398a851b6
Benjamin Blundell <me@benjamin.computer>
2021-05-28 18:41:33 +0100

    almost there

--- src/main.cc
+++ src/main.cc
@@ -46,6 +46,58 @@ bool ProcessTiff(Options &options, std::string &tiff_path, std::string &log_path
     std::vector<std::string> lines = util::ReadFileLines(log_path);
     image::LoadTiff<vkn::ImageU16L>(tiff_path, image);
 
+    size_t idx = 0;
+    std::vector<std::vector<size_t>> neurons; // 0: ASI-1, 1: ASI-2, 2: ASJ-1, 3: ASJ-2
+
+    // Read the log file and extract the     
+    for (int i = 0; i < 4; i++) {
+        neurons.push_back(std::vector<size_t>());
+    }
+
+    for (std::string line : lines) {
+        std::vector<std::string> tokens = util::SplitStringWhitespace(line);
+        if (util::ToLower(tokens[0]) == "associate") {
+            idx += 1;
+        } else {
+            neurons[idx-1].push_back(util::FromString<size_t>(tokens[0]));
+        }
+    }
+
+    // Export ASI-1 first. We split the tiffs so they have layers
+    vkn::ImageU8L3D asi1;
+    int neuron_id = 0;
+    asi1.width = image.width;
+    asi1.depth = 51;
+    asi1.height = image.height / asi1.depth ;
+
+    for (uint32_t d = 0; d < asi1.depth; d++) {
+        asi1.image_data.push_back(std::vector<std::vector<uint8_t>>());
+
+        for (uint32_t y = 0; y < asi1.height; y++) {
+            asi1.image_data[d].push_back(std::vector<uint8_t>());
+
+            for (uint32_t x = 0; x < asi1.width; x++) {
+                uint16_t val = image.image_data[d * asi1.height + y][x];
+                if (val == 0) {
+                    asi1.image_data[d][y].push_back(0);
+                } else {
+                    std::vector<size_t>::iterator it = std::find(neurons[neuron_id].begin(),
+                        neurons[neuron_id].end(), static_cast<size_t>(val));
+                    if (it != neurons[neuron_id].end()) {
+                        asi1.image_data[d][y].push_back(1);
+                    } else {
+                        asi1.image_data[d][y].push_back(0);
+                    }
+                }
+            }
+        }
+    }
+
+    std::vector<std::string> tokens_log = util::SplitStringChars(util::FilenameFromPath(log_path), "_.");
+    std::string image_id = tokens_log[0];
+    std::string output_path = options.output_path + "/" + image_id + "_asi1.tiff";
+    image::SaveTiff(output_path, asi1);
+
     return true;
 }
 
