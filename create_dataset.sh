7675105568eafa5167164b8e1c59944d99e4aa7f
Benjamin Blundell <me@benjamin.computer>
2021-07-23 15:17:17 +0100

    Added a create_dataset script

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # Usage ./create_dataset.sh <source> <dest>
 touch flatten.log
+COUNT=0
+echo $COUNT >> count.log
 
 for d in `ls $1` ; do
     echo $1/$d
+    ./build/flatten -m -r -n $COUNT -i $1/$d -o $2 >> flatten.log
     COUNT=`ls $2 | wc -l`
+    ((COUNT=COUNT+1))
     echo $COUNT
     echo $COUNT >> count.log
-    ./build/flatten -m -r -n $COUNT -i $1/$d -o $2 >> flatten.log
 done
