499e7fde3cb29699c40e959b30e1508785e13e27
Benjamin Blundell <me@benjamin.computer>
2021-07-23 16:03:03 +0100

    Added a create_dataset script

--- create_dataset.sh
+++ create_dataset.sh
@@ -8,7 +8,7 @@ echo $COUNT >> count.log
 
 for d in `ls $1` ; do
     echo $1/$d
-    ./build/flatten -m -r -n $COUNT -i $1/$d -o $2 >> flatten.log
+    ./build/flatten -m -r -b -n $COUNT -i $1/$d -o $2 >> flatten.log
     COUNT=`ls $2 | wc -l`
     ((COUNT=COUNT+1))
     echo $COUNT
