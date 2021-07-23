9980e2732f4aa61a8e10cda01ad9af3e82389f8e
Benjamin Blundell <me@benjamin.computer>
2021-07-23 15:32:52 +0100

    Added a create_dataset script

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Usage ./create_dataset.sh <source> <dest>
+# Usage ./create_dataset.sh <source> <dest> <annotations>
+
+# Flatten to get our mips
 touch flatten.log
 COUNT=0
 echo $COUNT >> count.log
@@ -11,4 +13,16 @@ for d in `ls $1` ; do
     ((COUNT=COUNT+1))
     echo $COUNT
     echo $COUNT >> count.log
+done
+
+# Now lets have a look at the dataset
+mapfile -t counts < count.log
+touch dataset.log
+IDX=0
+
+for d in in `ls $3` ; do
+    COUNT=${arr[$IDX]}
+    echo $COUNT $1/$d
+    ./build/dataset -f -r -n $COUNT -i $1/$d -o $2 >> dataset.log
+    ((IDX=IDX+1))
 done
