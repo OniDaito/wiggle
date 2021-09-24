75c76352f8365d85c5d40360bb9226e4f4b8331d
Benjamin Blundell <me@benjamin.computer>
2021-09-24 17:00:53 +0100

    Fix cherry2

--- multi_cherry2.sh
+++ multi_cherry2.sh
@@ -1,54 +1,54 @@
 #!/bin/sh
-./build/multiset -r -n 778 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 810 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 846 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 895 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 948 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1002 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1055 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1105 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1155 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1206 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1256 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1309 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1363 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1408 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1461 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1504 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1551 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1608 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1660 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1742 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1792 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1843 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1893 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1943 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 1993 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2047 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2101 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2152 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2205 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2265 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2317 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2377 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2434 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2485 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2516 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2574 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2634 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2676 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2726 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2777 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2833 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2884 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2935 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 2989 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL417-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3041 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL787-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3092 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL795-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3142 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180208-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3178 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3220 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3270 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3327 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3389 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
-./build/multiset -r -n 3428 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021  >> dataset.log
+./build/multiset -r -n 0 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 32 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 68 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 117 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170810-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 170 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 224 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 277 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 327 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170817-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 377 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 428 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL603-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 478 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL806-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 531 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Hai_analysis/20170818-QL867-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 585 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 630 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 683 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170821-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 726 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 773 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 830 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170825-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 882 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 964 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL569-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1014 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20170828-QL849-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1065 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1115 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1165 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1215 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1269 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170907-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1323 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1374 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1427 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1487 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1539 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170908-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1656 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL285-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1707 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL568-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1738 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL823-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1796 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL824-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1856 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/LH\ ARZ\ analysis/20170911-QL835-d1.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1898 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1948 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 1999 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180126-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2055 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2106 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2157 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180201-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2211 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2263 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL417-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2314 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL787-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2364 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180202-QL795-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2390 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180208-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2442 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2492 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2549 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180302-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2611 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL285-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2650 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL569-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
+./build/multiset -r -n 2698 -i /phd/wormz/queelim/ins-6-mCherry_2/Annotations/Reesha_analysis/20180308-QL849-d0.0 -o /phd/wormz/queelim/dataset_24_09_2021 >> dataset.log
