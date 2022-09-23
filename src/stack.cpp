a297fe22e98fc50c4f8c5d249b27e9198c74f3da
Benjamin Blundell <me@benjamin.computer>
2022-09-23 14:30:56 +0100

    Added the stacker

--- src/stack.cpp
+++ src/stack.cpp
@@ -123,7 +123,7 @@ int main (int argc, char ** argv) {
     int option_index = 0;
     int image_idx = 0;
 
-    while ((c = getopt_long(argc, (char **)argv, "i:o:l:p:tz:w:h:s:?", long_options, &option_index)) != -1) {
+    while ((c = getopt_long(argc, (char **)argv, "i:o:l:p:tbz:w:h:s:?", long_options, &option_index)) != -1) {
         switch (c) {
             case 0 :
                 break;
