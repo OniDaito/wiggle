13173ed012713bf483b8dac9193d22975271d653
Benjamin Blundell <me@benjamin.computer>
2022-10-20 15:25:22 +0100

    added an autonoise for 2D

--- scripts/genpairs.py
+++ scripts/genpairs.py
@@ -34,6 +34,53 @@ if __name__ == "__main__":
                 for line in lines:
                     if "Renaming" in line:
                         tokens = line.split(" to ")
-                        original = tokens[0].replace("Renaming ","")[:-1]
+                        original = tokens[0].replace("Renaming ","")
                         derived = tokens[1][1:].replace("\n","")
                         w.write(original + "," + derived + "\n")
+        
+        with open(args.dataset + "/pairs_fits.csv", "w") as w:
+            w.write("original,derived\n")
+
+            with open(args.dataset + "/dataset.log", 'r') as f:
+                lines = f.readlines()
+
+                for line in lines:
+                    if "Renaming" in line:
+                        tokens = line.split(" to ")
+                        original = tokens[0].replace("Renaming ","")
+
+                        original = original.replace("ins-6-mCherry/", "mcherry_fits/")
+                        original = original.replace("ins-6-mCherry_2/", "mcherry_2_fits/")
+                        original = original.replace(".tiff", ".fits")
+
+                        derived = tokens[1][1:].replace("\n","")
+                        w.write(original + "," + derived + "\n")
+
+        with open(args.dataset + "/rois.csv", "w") as w:
+            w.write("filename,x,y,z,xydim,zdim\n")
+
+            with open(args.dataset + "/dataset.log", 'r') as f:
+                lines = f.readlines()
+                current_file = ""
+
+                for line in lines:
+                    if "Stacking" in line:
+                        current_file = line.replace("Stacking: ","").replace("\n","") 
+
+                    if "ROI:" in line and current_file != "":
+                        tokens = line.split(",")
+                        x = int(tokens[0].replace("ROI: ", ""))
+                        y = int(tokens[1].replace(" ", ""))
+                        z = int(tokens[2].replace(" ", ""))
+                        xydim = int(tokens[3].replace(" ", ""))
+                        zdim = int(tokens[4].replace(" ", "").replace("\n",""))
+                        w.write(current_file + "," + str(x) + "," + str(y) + "," + str(z) + "," + str(xydim) + "," + str(zdim) + "\n")
+                    elif ",ROI," in line:
+                        tokens = line.split(",")
+                        current_file = tokens[0]
+                        x = int(tokens[2])
+                        y = int(tokens[3])
+                        z = int(tokens[4])
+                        xydim = int(tokens[5])
+                        zdim = int(tokens[6])
+                        w.write(current_file + "," + str(x) + "," + str(y) + "," + str(z) + "," + str(xydim) + "," + str(zdim) + "\n")
