e9596df082dceb5bb41be80a1ab9e48507f00fdd
Benjamin Blundell <me@benjamin.computer>
2022-10-21 14:44:56 +0100

    Added an optimiser for finding the worm centroid

---
+++ src/test/basemodel.cpp
@@ -0,0 +1,14 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "test/doctest.h"
+#include "basemodel.hpp"
+
+
+TEST_CASE("Testing Basemodel solver") {
+    // NeuronDists dists = {52.2398, 51.517, 43.7836, 43.7836, 47.55, 47.676, 47.5815}; // Distances already computed from the DB
+
+    NeuronDists dists = {0.5, 0.5, 0.6, 0.5, 0.6, 0.5 }; // Distances already computed from the DB
+
+
+
+
+}
